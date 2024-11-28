1a8b4d1cdd49fbb8e662f586e573b6460eb70441
Iván Ovejero <ivov.src@gmail.com>
2024-11-28 17:25:30 +0100

    Simplify other as well

--- internal/http/monitor_runner_health_test.go
+++ internal/http/monitor_runner_health_test.go
@@ -107,10 +107,8 @@ func TestMonitorRunnerHealthFailure(t *testing.T) {
 	}
 
 	defer func() {
-		if cmd.Process != nil {
-			if err := cmd.Process.Kill(); err != nil {
-				t.Logf("Failed to kill process during cleanup: %v", err)
-			}
+		if cmd.Process != nil && cmd.Process.Kill() != nil {
+			t.Log("Failed to kill process during cleanup")
 		}
 	}()
 
